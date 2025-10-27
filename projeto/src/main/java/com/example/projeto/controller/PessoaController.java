48d832641520dac2de0797d706c03903c692e59d
luís henrique <103255891+luishsc11@users.noreply.github.com>
2025-10-27 12:38:36 +0000

    agora vai disnara

--- projeto/src/main/java/com/example/projeto/controller/PessoaController.java
+++ projeto/src/main/java/com/example/projeto/controller/PessoaController.java
@@ -9,12 +9,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;  
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.CrossOrigin;   
+import org.springframework.web.bind.annotation.CrossOrigin;
+   
 
 import com.example.projeto.service.PessoaService;
 import com.example.projeto.model.Pessoa;
 
-@AllowOrigin("https://didactic-invention-xgg44r5755xc6vg6-3000.app.github.dev")
+//@AllowOrigin("https://didactic-invention-xgg44r5755xc6vg6-3000.app.github.dev")
+@CrossOrigin(origins = "https://didactic-invention-xgg44r5755xc6vg6-3000.app.github.dev")
 @RestController
 @RequestMapping("/api/pessoas")
 public class PessoaController{
